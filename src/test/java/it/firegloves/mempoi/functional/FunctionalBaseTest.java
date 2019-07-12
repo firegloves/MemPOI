d31cb664078a29e228a46551dd0a25942b7634ac
Luca Corsetti <luca.corsetti@sourcesense.com>
2019-07-12 10:51:13 +0200

    test db dump

--- src/test/java/it/firegloves/mempoi/functional/FunctionalBaseTest.java
+++ src/test/java/it/firegloves/mempoi/functional/FunctionalBaseTest.java
@@ -14,7 +14,7 @@ public abstract class FunctionalBaseTest {
 
     protected File outReportFolder = new File("out/report-files/");
 
-    // in order to run tests you need to manually import resources/mempoi_export_test.sql first
+    // in order to run tests you need to manually import resources/test_dump.sql first
     // it's a mysql 8+ dump
     // then adjust db connection string accordingly with your parameters
 
@@ -26,9 +26,6 @@ public abstract class FunctionalBaseTest {
         try {
             this.conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/mempoi", "root", "");
             this.prepStmt = this.conn.prepareStatement("SELECT id, creation_date AS `WONDERFUL DATE`, dateTime, timeStamp, name, valid, usefulChar, decimalOne FROM mempoi.export_test LIMIT 0, 10");
-//            this.prepStmt = this.conn.prepareStatement("SELECT id, creation_date AS `WONDERFUL DATE`, dateTime, timeStamp, name, valid, usefulChar, decimalOne, bitTwo, doublone, floattone, interao, mediano, attempato, interuccio " +
-//                    "FROM mempoi.export_test");
-
 
             if (! this.outReportFolder.exists() && ! this.outReportFolder.mkdirs()) {
                 throw new MempoiRuntimeException("Error in creating out report file folder: " + this.outReportFolder.getAbsolutePath() + ". Maybe permissions problem?");
